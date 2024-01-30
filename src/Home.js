eed149e9ae27dfb917088ebbeb98a024d286dfd7
Karen Jo <karenfiksdahl@gmail.com>
2024-01-30 11:29:35 +0100

    adds bloglists, styling

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,20 @@ import { useState } from 'react';
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'New blog', body: 'lorem ipsum..', author: 'Jay', id: 1 },
-        { title: 'New blog', body: 'lorem ipsum..', author: 'Kay', id: 2 },
-        { title: 'New blog', body: 'lorem ipsum..', author: 'May', id: 3 }
-
+        { title: 'Cool blog', body: 'lorem ipsum..', author: 'Kay', id: 2 },
+        { title: 'Hello blog', body: 'lorem ipsum..', author: 'May', id: 3 }
     ]);
 
     return (  
         <div className="home">
             {blogs.map((blog) => {
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author} </p>
-                </div>
+                return (
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author} </p>
+                    </div>
+                );
             })}
-            
         </div>
     );
 }
