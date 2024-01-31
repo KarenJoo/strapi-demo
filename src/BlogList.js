361ed4d32b3e706a7d182811d5d783a0a36ae454
Karen Jo <karenfiksdahl@gmail.com>
2024-01-31 13:57:08 +0100

    adds content from db.json to BlogDetails page

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,16 +1,17 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const BlogList = ({ blogs, title, handleDelete }) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
-
-
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
                         {blogs.map((blog) => {
                 return (
                     <div className="blog-preview" key={blog.id}>
+                        <Link to={`/blogs/${blog.id}`}> 
                         <h2>{blog.title}</h2>
                         <p>Written by {blog.author} </p>
+                        </Link>
+                       
                           <img src={blog.image} alt={blog.title} />
                        
                     </div>
