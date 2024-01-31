cd9d5adb3ecba750d4c1e5e43f8f249c9dfd67f2
Karen Jo <karenfiksdahl@gmail.com>
2024-01-31 20:06:38 +0100

    adds delete request and delete btn on blogpost

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,29 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
 
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to delete blog');
+            }
+            return response.json();
+        })
+        .then(() => {
+            history.push('/');
+        })
+        .catch(error => {
+            console.error('Error deleting blog:', error);
+        });
+    };
+    
 
     return ( 
         <div className="blog-details">
@@ -16,6 +35,7 @@ const BlogDetails = () => {
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
                     <img src={blog.image} alt="Blog Image" /> 
+                    <button onClick={handleClick}>Delete blog</button>
                 </article>
             )}
         </div>
